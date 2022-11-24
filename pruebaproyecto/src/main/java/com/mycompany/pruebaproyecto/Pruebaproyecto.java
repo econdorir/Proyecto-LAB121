f93841515afbf205a0507a3ef42ad60361c541ae
econdorir <118948322+econdorir@users.noreply.github.com>
2022-11-23 22:03:31 -0400

    asas

---
+++ pruebaproyecto/src/main/java/com/mycompany/pruebaproyecto/Pruebaproyecto.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.pruebaproyecto;
+
+
+public class Pruebaproyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        System.out.println("Errores");
+    }
+}
